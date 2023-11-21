db935065d3893c75370718c95fbb43be52a6344e
Slava30041991 <balahenka30041991@gmail.com>
2023-11-21 22:16:51 +0300

    Устранены ошибки базового класса

---
+++ src/test/java/helpDesk/HelpDeskTest.java
@@ -0,0 +1,11 @@
+package helpDesk;
+
+import core.BaseTestSelenium;
+import org.junit.Test;
+
+public class HelpDeskTest extends BaseTestSelenium {
+    @Test
+    public void checkTicket(){
+
+    }
+}
